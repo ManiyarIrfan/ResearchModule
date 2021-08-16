abd95bfb4f2743adeb9a22b4e364217804e0b021
Irfan Maniyar <maniyar.irfan17@gmail.com>
2021-08-16 22:03:37 +0530

    changes

--- src/app/layout/scientist-layout/dashboard/dashboard.module.ts
+++ src/app/layout/scientist-layout/dashboard/dashboard.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
-// import { ScientistLayoutModule } from '../scientist-layout.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    DashboardRoutingModule,
-    // ScientistLayoutModule
+    DashboardRoutingModule
   ]
 })
 export class DashboardModule { }
